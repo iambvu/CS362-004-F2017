1722fdf0b7b2e39fdfeb7f2b7323825e32798e23
iambvu <vub@oregonstate.edu>
2017-10-06 15:53:16 -0700

    assignment-2 refactor

--- projects/vub/dominion/dominion_helpers.h
+++ projects/vub/dominion/dominion_helpers.h
@@ -11,5 +11,10 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
+int adventurer_card(int drawntreasure, struct gameState *state, int currentPlayer, int *temphand);
+int smithy_card(struct gameState *state, int currentPlayer, int handPos);
+int village_card(struct gameState *state, int currentPlayer, int handPos);
+int great_hall_card(struct gameState *state, int currentPlayer, int handPos);
+int mine_card(struct gameState *state, int currentPlayer, int choice1, int choice2, int handPos);
 
 #endif
